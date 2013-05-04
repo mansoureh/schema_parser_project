ba7f49bc002ff4f21aa350184ca1ffe3979b2c22
Joshua Wulf <jwulf@redhat.com>
2013-05-04 15:18:05 +1000

    Initial commit of tests

---
+++ spec/md-parser.spec.js
@@ -0,0 +1,21 @@
+require mdparser = ('schemaParser_1');
+
+describe ('Microdata Parser', function () {
+    var smallTestFile = 'lib/smallfile.html',
+        bigTestFile = 'lib/bigfile.html';
+
+
+   it('should correctly parse a small file',
+   function (done) {
+       var _fs
+       mdparser.parseSchemaOrg()
+
+   });
+
+   it('should correctly parse a large file',
+   function(done) {
+
+   });
+
+
+});
